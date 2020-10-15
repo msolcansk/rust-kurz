8e67626e3724c27055dd8ffda68999d41bf3059c
firestormse <solcansky.martin@gmail.com>
2020-10-15 16:57:38 +0200

    add fn ask_user

--- du2/src/main.rs
+++ du2/src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
+fn ask_user() -> String {
+    let reader = io::stdin();
+    let mut buffer: String = String::new();
+
+    reader.read_line(&mut buffer)
+        .ok()
+        .expect("ERRMSG");
+
+    println!("Janko Ukradol {}!", buffer);
+    return buffer;
+}
+
 fn main() {
-    println!("Hello, world!");
+    
+    let stolen_apples = ask_user();
+    println!("Pocet Ukradnutych jablk: {}", stolen_apples);
+   
+    
 }
